78cf4160cd5f7f37bf75e322fec48887fd1dc39b
Vallasc <vallasc@protonmail.com>
2023-02-22 12:13:49 +0100

    Update

---
+++ Assets/Scripts/Utils/HeadGaze.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadGaze : MonoBehaviour
+{
+    private GameObject lastFocusedObject = null;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        RaycastHit hitInfo;
+        if (Physics.Raycast(
+                Camera.main.transform.position,
+                Camera.main.transform.forward,
+                out hitInfo,
+                20.0f,
+                Physics.DefaultRaycastLayers))
+        {
+            // If the Raycast has succeeded and hit a hologram
+            // hitInfo's point represents the position being gazed at
+            // hitInfo's collider GameObject represents the hologram being gazed at
+            lastFocusedObject = hitInfo.transform.gameObject;
+        }
+    }
+
+    public GameObject GetLastFocusedObject()
+    {
+        GameObject gameObject = lastFocusedObject;
+        while (gameObject.transform.parent != null && !gameObject.name.Equals("PdfSearch") && !gameObject.name.Equals("PdfView(Clone)"))
+            gameObject = gameObject.transform.parent.gameObject;
+        return gameObject;
+    }
+}
---
+++ Assets/Scripts/Utils/SpeechCommandsHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechCommandsHandler : MonoBehaviour
+{
+    public HeadGaze headGaze;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CloseLastFocused()
+    {
+        GameObject last = headGaze.GetLastFocusedObject();
+        if (last.name.Equals("PdfView(Clone)"))
+            last.SetActive(false);
+    }
+}
